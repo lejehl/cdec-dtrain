864afe79bb948d8bb3eafe99af71fc86e944125c
Laura Jehl <jehl@cl.uni-heidelberg.de>
2013-05-10 11:39:39 +0200

    viterbi translator for initialisation

---
+++ training/dtrain/viterbiget.h
@@ -0,0 +1,47 @@
+/*
+ * viterbiget.h
+ *
+ *  Created on: 6 May 2013
+ *      Author: jehl
+ */
+
+#ifndef VITERBIGET_H_
+#define VITERBIGET_H_
+
+#include "viterbi.h"
+#include "dtrain.h"
+
+namespace dtrain
+{
+
+struct ViterbiGetter : public HypSampler
+{
+
+	vector<WordID> transl_;
+
+	ViterbiGetter():transl_() {};
+	virtual void
+	  NotifyTranslationForest(const SentenceMetadata& smeta, Hypergraph* hg)
+	  {
+
+			transl_ = getViterbiTranslation(*hg);
+	  }
+
+	vector<WordID> getViterbiTranslation( const Hypergraph& forest ){
+		vector<WordID> result;
+		ViterbiESentence( forest, &result );
+		return result;
+
+	}
+
+
+	// this will never be used
+	vector<ScoredHyp>* GetSamples() {
+		vector<ScoredHyp>* v;
+		return v;  };
+};
+
+}
+
+
+#endif /* VITERBIGET_H_ */
