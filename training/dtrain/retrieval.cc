08b87762f994a47791cc72945717adaf7ee37c6c
laura <laura.jehl@gmail.com>
2013-05-20 15:51:30 +0200

    retrieval for merge

--- training/dtrain/retrieval.cc
+++ training/dtrain/retrieval.cc
@@ -41,31 +41,57 @@ double RetrievalEval::averagePrecision( vector<unsigned>& gold, vector<unsigned>
 
 }
 
-double RetrievalEval::avPrecAtN( vector<unsigned>& gold, vector<unsigned>& retrieved, unsigned n ){
-	double avPrec = 0.0;
-	if  (  n > retrieved.size()  )  {
-		cerr << "n must be less than or equal to num_retrieved!" << endl;
-		exit(1);
-	} else if ( n == 0 ) {
-		n = gold.size();
-	} else {
-		if (n < gold.size() ) {
-				cout << "retrieved has " << retrieved.size() << " elements." << endl;
-				cout << "gold has " << gold.size() << " elements." << endl;
-				gold.erase( gold.begin()+n, gold.end() );
-		}
-		if ( n > gold.size() ) {
-			for ( unsigned i = gold.size(); i < n; i++ ){
-				gold.push_back( 0 );
-			}
-		}
+double RetrievalEval::ndcg( vector<unsigned>& retrieved ){
+	cout << "Scoring: ndcg" << endl;
+	double dcg = (double) retrieved[0];
+	vector<double> normalizers;
+	normalizers.push_back( 1.0 );
+	for (unsigned i=2; i <= retrieved.size(); i++){
+		// log(base 2) of rank
+		double normalizer = log ( i ) / log ( 2 );
+		dcg += (double) retrieved[i-1] / normalizer;
+		cout << "dcg@" << i << ": " << retrieved[i-1] / normalizer << endl;
+		// remember normalizer for dcg
+		normalizers.push_back( normalizer );
 	}
-	avPrec = averagePrecision( gold, retrieved );
-
-	return avPrec;
+	cout << "total dcg: " << dcg << endl;
+	sort ( retrieved.begin(), retrieved.end(), std::greater<unsigned>());
+	double idcg = 0.0;
+	for (unsigned i=1; i <= retrieved.size(); i++){
+		idcg += (double) retrieved[i-1] / normalizers[i-1];
+	}
+	cout << "total idcg: " << idcg << endl;
+	cout << "ndcg: " << dcg/idcg << endl;
+	return dcg/idcg;
 }
 
-Retrieval::Retrieval( string scoring, unsigned heap_size ){
+
+//double RetrievalEval::avPrecAtN( vector<unsigned>& gold, vector<unsigned>& retrieved, unsigned n ){
+//	double avPrec = 0.0;
+//	if  (  n > retrieved.size()  )  {
+//		cerr << "n must be less than or equal to num_retrieved!" << endl;
+//		exit(1);
+//	} else if ( n == 0 ) {
+//		n = gold.size();
+//	} else {
+//		if (n < gold.size() ) {
+//				cout << "retrieved has " << retrieved.size() << " elements." << endl;
+//				cout << "gold has " << gold.size() << " elements." << endl;
+//				gold.erase( gold.begin()+n, gold.end() );
+//		}
+//		if ( n > gold.size() ) {
+//			for ( unsigned i = gold.size(); i < n; i++ ){
+//				gold.push_back( 0 );
+//			}
+//		}
+//	}
+//	avPrec = averagePrecision( gold, retrieved );
+//
+//	return avPrec;
+//}
+
+Retrieval::Retrieval( string scoring, unsigned heap_size )
+: eval_(){
 	heap_size_ = heap_size;
 	scoring_ = scoring;
 	cout << "created new instance of Retrieval" << endl <<
@@ -108,9 +134,9 @@ double Retrieval::evaluateRetrieval( map<string, unsigned>& rels, MyHeap& result
 	if (scoring_ == "map"){
 		vector<unsigned> gold;
 		getSortedRelevances( gold, rels );
-		score =  averagePrecision(  gold, retrieved );
+		score =  eval_.averagePrecision(  gold, retrieved );
 	} else {
-		score = ndcg( retrieved );
+		score = eval_.ndcg( retrieved );
 	}
 	cout << "\t" << score << endl;
 	return score;
@@ -126,48 +152,29 @@ void Retrieval::getSortedRelevances( vector<unsigned>& gold, map<string, unsigne
 /*
  * Average Precision
  */
-double Retrieval::averagePrecision( vector<unsigned>& gold, vector<unsigned>& retrieved ){
-	unsigned counter = 0;
-	double sum = 0.0;
-
-	// precision at i
-	cout << "relevance (gold)\tsum(precision@rank)" << endl;
-	for ( unsigned i = 0; i < gold.size(); i++ ){
-		if ( retrieved.at(i) >= gold.at(i) ){
-			counter++;
-			sum += (double) counter / (double)( i+1 );
-		}
-		cout << retrieved.at(i) << " (" << gold.at(i) << ") " ;
-	}
-	cout << "\t" << sum;
-	double avPrec;
-
-	// normalize by number of RELEVANT docs
-	avPrec = sum/gold.size();
-//	cout << "\t" << avPrec;
-	cout << endl;
-	return avPrec;
-}
+//double Retrieval::averagePrecision( vector<unsigned>& gold, vector<unsigned>& retrieved ){
+//	unsigned counter = 0;
+//	double sum = 0.0;
+//
+//	// precision at i
+//	cout << "relevance (gold)\tsum(precision@rank)" << endl;
+//	for ( unsigned i = 0; i < gold.size(); i++ ){
+//		if ( retrieved.at(i) >= gold.at(i) ){
+//			counter++;
+//			sum += (double) counter / (double)( i+1 );
+//		}
+//		cout << retrieved.at(i) << " (" << gold.at(i) << ") " ;
+//	}
+//	cout << "\t" << sum;
+//	double avPrec;
+//
+//	// normalize by number of RELEVANT docs
+//	avPrec = sum/gold.size();
+////	cout << "\t" << avPrec;
+//	cout << endl;
+//	return avPrec;
+//}
 
 /*
  * normalized discounted cumulative gain
  */
-double Retrieval::ndcg( vector<unsigned>& retrieved ){
-	cout << "Scoring: ndcg" << endl;
-	double dcg = (double) retrieved[0];
-	vector<double> normalizers;
-	normalizers.push_back( 1.0 );
-	for (unsigned i=2; i <= retrieved.size(); i++){
-		// log(base 2) of rank
-		double normalizer = log ( i ) / log ( 2 );
-		dcg += (double) retrieved[i-1] / normalizer;
-		// remember normalizer for dcg
-		normalizers.push_back( normalizer );
-	}
-	sort ( retrieved.begin(), retrieved.end(), std::greater<unsigned>());
-	double idcg = 0.0;
-	for (unsigned i=1; i <= retrieved.size(); i++){
-		idcg += (double) retrieved[i-1] / normalizers[i-1];
-	}
-	return dcg/idcg;
-}
